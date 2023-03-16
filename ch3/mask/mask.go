b13806751c291c7c9cccde16385702bbeea54735
George Onwuasoanya <georgetheprogrammer@gmail.com>
2023-03-16 13:08:30 +0100

    IPMask for network address given the ones and bits value

---
+++ ch3/mask/mask.go
@@ -0,0 +1,37 @@
+/*
+IP Mask
+*/
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		log.Fatalf("usage: %s dotted-ip-address ones bits \n", os.Args[0])
+	}
+	dotIp := os.Args[1]
+	ones, _ := strconv.Atoi(os.Args[2])
+	bits, _ := strconv.Atoi(os.Args[3])
+	addrs := net.ParseIP(dotIp)
+
+	if addrs == nil {
+		log.Fatal("invalid address")
+	}
+
+	//The simplest function to create a netmask uses the CIDR notation of ones followed by zeroes up to the number of bits:
+	mask := net.CIDRMask(ones, bits) // Classless Inter-Domain Routing (a.k.a. CIDR)
+	computedOnes, computedBits := mask.Size()
+	network := addrs.Mask(mask)
+
+	fmt.Println("Address is ", addrs.String(),
+		"\nMask Length is : ", computedBits,
+		"\nLeading ones count is : ", computedOnes,
+		"\nMask is (hex): ", mask.String(),
+		"\nNetwork is : ", network.String())
+}
